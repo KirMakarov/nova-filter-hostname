9a9c23ede592193f379b000f4197f4076ac6eef9
Kirill Makarov <k.a.makarov@gmail.com>
2022-02-07 00:29:41 +0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='nova-filter-hostname',
+    version='0.1',
+    description='Сustom filter by hostname',
+    packages=[
+        'nova_filter_hostname'
+    ],
+)
